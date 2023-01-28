40d72fe5092f5b54ac387c782f86a853f76ca1c7
LaFleur66 <120199703+LaFleur66@users.noreply.github.com>
2023-01-28 03:42:03 +0100

    Update app.R
    final version

--- Bilanconso/app.R
+++ Bilanconso/app.R
@@ -16,55 +16,15 @@ library(dplyr)
 library(stringr)
 library(tidyr)
 
-prod_commune <- read.csv2("production-electrique-par-filiere-a-la-maille-commune.csv")
-prod_region <- read.csv2("production-electrique-par-filiere-a-la-maille-region.csv")
-prod_departement <- read.csv2("production-electrique-par-filiere-a-la-maille-departement.csv")
-
-conso_commune <- read.csv2("consommation-electrique-par-secteur-dactivite-commune.csv")
-conso_region <- read.csv2("consommation-electrique-par-secteur-dactivite-region.csv")
-conso_departement <- read.csv2("consommation-electrique-par-secteur-dactivite-departement.csv")
-
-cleanames <- function(x){
-  p<-colnames(x)
-  p<-str_to_lower(p)
-  p<-str_trim(p)
-  p<-str_replace_all(p, pattern = "[éèê]+", replacement = "e")
-  p<-str_replace_all(p, pattern = "[.]+", replacement = "_")
-  colnames(x)<-p
-  return(x)
-} 
-
-prod_commune <- cleanames(prod_commune)
-prod_region <- cleanames(prod_region)
-prod_departement <- cleanames(prod_departement)
-
-conso_commune <- cleanames(conso_commune)
-conso_region <- cleanames(conso_region)
-conso_departement <- cleanames(conso_departement)
 
 
-cleanames_column <- function(x){
-  p <- x
-  p<-str_to_lower(p)
-  p<-str_trim(p)
-  p<-str_replace_all(p, pattern = "[éèê]+", replacement = "e")
-  p<-str_replace_all(p, pattern = "[.]+", replacement = "_")
-  p<-str_replace_all(p, pattern = " ", replacement = "-")
-  p<-str_replace_all(p, pattern = "î", replacement = "i")
-  p<-str_replace_all(p, pattern = "ô", replacement = "o")
-  return(p)
-}
+prod_commune <- readRDS("prod_commune_clean.rds")
+prod_region <- readRDS("prod_region_clean.RDS")
+prod_departement <- readRDS("prod_departement_clean.rds")
 
-prod_commune$nom_commune <- cleanames_column(prod_commune$nom_commune)
-prod_region$nom_region <- cleanames_column(prod_region$nom_region)
-prod_departement$nom_departement<- cleanames_column(prod_departement$nom_departement)
-
-conso_commune$nom_commune <- cleanames_column(conso_commune$nom_commune)
-conso_region$nom_region <- cleanames_column(conso_region$nom_region)
-conso_departement$nom_departement <- cleanames_column(conso_departement$nom_departement)
-
-
-
+conso_commune <- readRDS("conso_commune_clean.rds")
+conso_region <- readRDS("conso_region_clean.rds")
+conso_departement <- readRDS("conso_departement_clean.rds")
 
 list_conso <- list(conso_commune,conso_departement,conso_region)
 list_prod <- list(prod_commune,prod_departement,prod_region)
@@ -74,7 +34,7 @@ names(list_prod) <- c("commune","departement","region")
 
 
 vec_maille <- c("region","departement","commune")
-vec_annee <- unique(conso_commune$annee)
+vec_annee <- unique(conso_region$annee)
 
 # Define UI for application that draws a histogram
 ui <- dashboardPage(
@@ -88,7 +48,7 @@ ui <- dashboardPage(
         menuItem("Résumé", tabName = "resume", icon = icon("dashboard")),
         menuItem("Détail", tabName = "details", icon = icon("th"))
       ),
-      selectInput("maille_interet", "Maille d'interet", choices = vec_maille),
+      selectInput("maille_interet", "Maille d'interet", choices = vec_maille, selected = "region"),
       selectInput("annee", "Années d'interet", multiple = T, choices = NULL),
       selectInput("maille_vis","Maille",multiple = T, choices = NULL)
     ),
@@ -149,8 +109,8 @@ server <- function(session,input, output) {
     observe({
       annee <- conso() %>% dplyr::select(annee)
       maille_vis<- conso() %>% dplyr::select(sprintf("nom_%s", input$maille_interet))
-      updateSelectInput(session,"annee","annee(s) d'interet",choices = unique(annee))
-      updateSelectInput(session,"maille_vis","mailles",choices = unique((maille_vis)))
+      updateSelectInput(session,"annee","annee(s) d'interet",choices = unique(annee), selected = as.character(c("2020","2021")))
+      updateSelectInput(session,"maille_vis","mailles",choices = unique((maille_vis)), selected = "moyenne-france")
     })
     
     conso1 <- reactive({ conso() %>% filter(.[[sprintf("nom_%s", input$maille_interet)]] %in% input$maille_vis & annee %in% input$annee)})
@@ -161,28 +121,43 @@ server <- function(session,input, output) {
     # Onglet Résumé 
     
     ## Graphique avec une barre sur la conso et une barre sur la prod
-  
-    t1 <- reactive({ apply(prod1()
-                     %>% select(contains("mwh")),
-                     MARGIN = 2, FUN = as.numeric ) 
-                  })
     
-    sum_prod <- reactive({ apply(t1(), 2, sum, na.rm= T)*0.000001 })
+    sum_prod <- reactive({ apply(prod1() %>% select(prod_tot), 2, sum, na.rm= T)*0.000001 })
     
     energie <- c("photovoltaique","eolien","hydraulique","cogeneration","bio_energie","autres")
     
     prod_energie <- reactive({ data.frame(cbind(value = as.numeric(sum_prod()),energie = energie)) })
     
+    data_barplotconso <- reactive ({
+      data_barplot_conso <- conso1() %>% group_by(annee,code_grand_secteur,.[[sprintf("nom_%s", input$maille_interet)]]) %>%
+      summarise(consotot=sum(as.numeric(conso_totale_mwh_),na.rm = T)) %>%
+      mutate(consototTWH=consotot*0.000001)
+      colnames(data_barplot_conso) <-  c("annee","code_grand_secteur",sprintf("nom_%s", input$maille_interet),"consotot","consototTWH") 
+      
+      data_barplot_conso
+      
+      })
+    
+    data_barplotprod <- reactive ({
+      data_barplot_prod <- prod1() %>% group_by(annee,type_prod,.[[sprintf("nom_%s", input$maille_interet)]]) %>%
+      summarise(prodtot=sum(as.numeric(prod_tot),na.rm = T))%>%
+      mutate(prodtotTWH=prodtot*0.000001)
+      colnames(data_barplot_prod) <- c("annee","type_prod",sprintf("nom_%s", input$maille_interet),"prodtot","prodtotTWH")
+      
+      data_barplot_prod
+    })
+    
+    
     plot_bar_conso <- reactive({
-      ggplot(conso1() %>% group_by(annee,code_grand_secteur)
-             %>% summarise(consotot=sum(as.numeric(conso_totale_mwh_),na.rm = T))
-             %>% mutate(consototTWH=consotot*0.000001)) +
-
-        geom_bar(aes(x="conso", y=consototTWH,fill=code_grand_secteur), stat = 'identity') +
-        geom_bar(data = prod_energie(),
-                 aes(x="prod",y=as.numeric(value),colour = energie) 
+      ggplot() +
+        geom_bar(data = data_barplotconso(), aes(x="conso", y=consototTWH,fill=code_grand_secteur), stat = 'identity') +
+        geom_bar(data = data_barplotprod(),
+                 aes(x="prod",y=prodtotTWH,colour = type_prod) 
                  ,stat = 'identity')+
-        scale_fill_brewer(palette="Spectral") 
+        scale_fill_brewer(palette="Spectral") +
+        facet_wrap(as.formula(paste("~", sprintf("nom_%s", input$maille_interet))))+
+        labs(title = "Conso et prod par type",x="", y = "Conso et Prod en TWH")
+        
     })
     
     output$bar_conso <- renderPlot({plot_bar_conso()})
@@ -213,28 +188,23 @@ server <- function(session,input, output) {
     ## Graphique interactif de l’évolution des consos par segment et des productions
     ## par segment selon l’année
     
-    product <- reactive({ prod1() %>%
-      select("annee", sprintf("nom_%s", input$maille_interet),
-             sprintf("code_%s", input$maille_interet),
-             starts_with("energie_produite")) %>%
-      tidyr::pivot_longer(- c("annee", sprintf("nom_%s", input$maille_interet), sprintf("code_%s", input$maille_interet)),
-                          values_to = "prod_mwh",
-                          names_to = "type") %>%
-      mutate(type = type %>%
-               stringr::str_replace_all(pattern = "energie_produite_annuelle_|mwh|__", replacement = ""))
-    })
     
-    plot <- reactive({ggplot( conso1() %>% group_by(annee,code_grand_secteur)
-                   %>% summarise(consotot=sum(as.numeric(conso_totale_mwh_),na.rm = T))
-                   %>% mutate(consototTWH=consotot*0.000001))+
-      aes(x=annee,y=as.numeric(consototTWH), col = code_grand_secteur)+
-      geom_line()+
-      geom_line(data = product() 
-                  %>% group_by(annee,type)
-                  %>% summarise(prodtot=sum(as.numeric(prod_mwh),na.rm = T))
-                  %>% mutate(prodtotTWH=prodtot*0.000001),
-                  aes(x=annee, y = as.numeric(prodtotTWH) , col = type))+
-        scale_fill_brewer(palette="Spectral") 
+    
+    plot <- reactive({ggplot() +
+      geom_line(data = conso1() %>% group_by(annee,code_grand_secteur)
+                %>% summarise(consotot=sum(as.numeric(conso_totale_mwh_),na.rm = T))
+                %>% mutate(consototTWH=consotot*0.000001) %>%
+                  ungroup(),
+                aes(x=annee,y=as.numeric(consototTWH), col = code_grand_secteur)
+                )+
+      geom_line(data = prod1() 
+                  %>% group_by(annee,type_prod)
+                  %>% summarise(prodtot=sum(as.numeric(prod_tot),na.rm = T))
+                  %>% mutate(prodtotTWH=prodtot*0.000001) %>%
+                  ungroup(),
+                  aes(x=annee, y = as.numeric(prodtotTWH) , col = type_prod))+
+                  labs(title = "Evolution de conso et prod par type",x = "Année", y = "Conso et Prod en TWH")+
+                  scale_color_discrete(name = "Type de prod et de conso")  
     })
     
     
@@ -249,10 +219,20 @@ server <- function(session,input, output) {
     
     ## Dataframe conso
     
-    conso2 <- reactive({conso1() %>% 
-      group_by_(sprintf("nom_%s", input$maille_interet), "code_grand_secteur", "annee") %>%
-      summarise(conso = sum(as.numeric(conso_totale_mwh_))) %>% 
-      pivot_wider(names_from = sprintf("nom_%s", input$maille_interet), values_from = "conso") 
+    
+    conso2 <- reactive({
+      conso_table <- conso() %>% 
+      filter(.[[sprintf("nom_%s", input$maille_interet)]] %in% input$maille_vis | .[[sprintf("nom_%s", input$maille_interet)]] == "moyenne-france")%>%
+      filter(annee %in% input$annee) %>%
+      group_by(annee, .[[sprintf("nom_%s", input$maille_interet)]], code_grand_secteur) %>%
+      summarise(conso = sum(as.numeric(conso_totale_mwh_)))
+      
+      colnames(conso_table) <- c("annee",sprintf("nom_%s", input$maille_interet),"code_grand_secteur","conso")
+      
+      conso_table <- conso_table %>% pivot_wider(names_from = sprintf("nom_%s", input$maille_interet), values_from = "conso") 
+      
+      conso_table
+      
     })
     
     output$conso <- renderDataTable(conso2())
@@ -271,10 +251,18 @@ server <- function(session,input, output) {
     
     ## Dataframe prod
     
-    prod2 <- reactive({product() %>% 
-        group_by_(sprintf("nom_%s", input$maille_interet), "type", "annee") %>%
-        summarise(prod = sum(as.numeric(prod_mwh))) %>% 
-        pivot_wider(names_from = sprintf("nom_%s", input$maille_interet), values_from = "prod") 
+    prod2 <- reactive({
+        prod_table <- prod() %>% 
+        filter(.[[sprintf("nom_%s", input$maille_interet)]] %in% input$maille_vis | .[[sprintf("nom_%s", input$maille_interet)]] == "moyenne-france")%>%
+        filter(annee %in% input$annee) %>%
+        group_by(annee, .[[sprintf("nom_%s", input$maille_interet)]], type_prod) %>%
+        summarise(prod = sum(as.numeric(prod_tot)))
+      
+        colnames(prod_table) <- c("annee",sprintf("nom_%s", input$maille_interet),"type_prod","prod")
+       
+        prod_table <- prod_table %>% pivot_wider(names_from = sprintf("nom_%s", input$maille_interet), values_from = "prod") 
+        
+        prod_table
     })
     
     
